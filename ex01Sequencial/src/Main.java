759fb44f06b7401bd1d750cf2a517baae3655381
Murilo Medeiros <muriloomestre@live.com>
2021-10-27 15:07:41 -0300

    "Ex01 resolvido e detalhado o que era pra ser feito"

--- ex01Sequencial/src/Main.java
+++ ex01Sequencial/src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números*/
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y =sc.nextInt();
+        int soma = x+y;
+        System.out.println("SOMA = "+soma);
+
+
+        sc.close();
     }
 }
